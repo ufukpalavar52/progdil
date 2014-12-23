6186f831be06b6bbcf5f7942aacd95ae6ebfedf3
ufukpalavar52 <ufuk.palavar@bil.omu.edu.tr>
2014-12-23 20:20:47 +0200

    Yeni Kodlar Eklendi

--- RandomName.h
+++ RandomName.h
@@ -290,6 +290,43 @@ int checkWord(int number, char language[])
 	  "Zumrut",
 	  ""
 	};
-
 	
 }
+
+int random_name_producer(int number, char project_name[], char names[][MAX], char adjectives[][MAX], int namelen, int adjlen,int z)
+{
+	int i = 0, control, filecontrol;
+	char str[MAX];
+
+	FILE* Project_Name;
+
+	if ((Project_Name = fopen(project_name, "a" )) == NULL)
+		printf("Dosya Acilamadi");
+	else
+	{
+		srand((long)time(NULL));
+		
+		for ( ; i<number; )
+		{ 
+			str[0] = 0;  
+			strcat(str, adjectives[ (rand() % adjlen) ]);
+			strcat(str, "-" );
+			strcat(str, names[ (rand() % namelen) ]);
+			
+			control = string_control(str, string_list, i);
+			filecontrol = file_control(str, project_name);
+		    
+			if (control == 1 && filecontrol == 1)
+			{
+			   strcat(string_list[i],str);
+			   printf("\n%d. %s",i+1,str);
+			   fprintf(Project_Name,"\n%s",str);
+			   create_folder(str,z);
+			   i++;
+			}
+		}
+		
+		fclose(Project_Name);
+	}
+}
+
