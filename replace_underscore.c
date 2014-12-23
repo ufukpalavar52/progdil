b28d95e2fbcda93cbb2039eb24c33e4d2d943751
cihanhub <velicihan@gmail.com>
2014-12-23 14:39:29 +0200

    Update replace_underscore.c

--- replace_underscore.c
+++ replace_underscore.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <curses.h>
-
 #define MAX 100
 
 void replace_usws (char []);
@@ -9,41 +8,36 @@ void replace_usws (char []);
 int main() {
 
     char word[MAX];
-	int needle;
-	printf ("\n\x1B[1;44;37mMetin: ");
-	
-	needle = 0;
-	while (1) {
-
-		scanf ("%c", &word[needle]);
+    int needle;
+    printf ("\n\x1B[1;44;37mMetin: ");
+    
+    needle = 0;
+    while (1) {
+    	
+    	scanf ("%c", &word[needle]);
        	if (word[needle] == '\n') {
-
-		   word[needle] = '\0';
-		   break;
-
-		}
-
-		needle++;
-
+       		
+       		word[needle] = '\0';
+		break;
 	}
 
-	replace_usws (word);
-	return 0;
-
+		needle++;
+    }
+    
+    replace_usws (word);
+    return 0;
 }
 
 void replace_usws (char  word[]) {
-
+	
 	int length, needle;
 	length = strlen (word);
    	printf ("\n");
    	
 	for (needle = 0; needle < length; needle++) {
-
 		if (word[needle] == '_' && (needle > 0 && needle < length - 1 )) {
-
+			
 			word[needle] = ' ';
-
 		}
 
 		printf ("\x1B[1;42;37m%c", word[needle]);   
