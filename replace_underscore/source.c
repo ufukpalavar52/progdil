7fbdfab93c94eeb270944025acc6b47f5b0fa6bd
cihanhub <velicihan@gmail.com>
2014-12-24 00:30:13 +0200

    finalizing replace_underscore

--- replace_underscore/source.c
+++ replace_underscore/source.c
@@ -6,53 +6,53 @@ void replace_usws (char []);
 
 int main () {
 
-  char word [max];
-  printf ("\n\x1B[1;44;37mMetin: ");
-  scanf ("%[^\n]s", word);
-  replace_usws (word);
-  return ;
+        char word [max];
+        printf ("\n\x1B[1;44;37mMetin: ");
+        scanf ("%[^\n]s", word);
+        replace_usws (word);
+        return 0;
 
 }
 
 void replace_usws (char  word[]) {
 
-  int needle, wordstart, wordend;
-  needle, wordstart = 0;
-  wordend = strlen(word) - 1;
-	
-  for (needle = 0; wordstart < wordend; needle++) {
+        int needle, wordstart, wordend;
+        needle, wordstart = 0;
+        wordend = strlen(word) - 1;
 
-		if (word[needle] != '_') {
+        for (needle = 0; wordstart < wordend; needle++) {
+
+                if (word[needle] != '_') {
 			
 			wordstart = needle;
-      break;
+                        break;
 
 		}
 
-  }
+        }
 
 
-  for (needle = wordend; wordend > 0; needle--) {
+        for (needle = wordend; wordend > 0; needle--) {
 
-    if (word[needle] != '_') {
+                if (word[needle] != '_') {
       
-      wordend = needle;
-      break;
+                        wordend = needle;
+                        break;
 
-    }
+                }
 
-  }
+        }
 
-  for (needle = wordstart; needle <= wordend; needle++) {
+        for (needle = wordstart; needle <= wordend; needle++) {
 
-    if (word[needle] == '_') {
+                if (word[needle] == '_') {
 
-      word[needle] = ' ';
+                        word[needle] = ' ';
 
-    }
+                }
  
-  }
+        }
 
-  printf ("\x1B[1;42;37m%s\x1B[0m\n\n", word);
+        printf ("\x1B[1;42;37m%s\x1B[0m\n\n", word);
 
 }
