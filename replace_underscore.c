a379981989854ff10785d0911090829c12f27716
ufukpalavar52 <ufuk.palavar@bil.omu.edu.tr>
2014-12-24 00:16:10 +0200

    Update replace_underscore.c

--- replace_underscore.c
+++ replace_underscore.c
@@ -29,17 +29,15 @@ void replace_usws (char  word[])
 	length = strlen (word);
    	printf ("\n");
    	
-	for (i = 1; i < length-1; i++) {
+	for (i = 1; i < length-1; i++) 
 		if (word[i] == '_'  &&  word[i-1] != '_' ) {
 			word[i] = ' ';
 		}
 
-		//printf ("\x1B[1;42;37m%c", word[needle]);   
-	}
 
 	for (j = length-1; j > 0; j--)
 		if (word[j] == ' ' && ( word[j+1] == '_' || word[j-1] == '_' )) {
 			word[j] = '_';
 		 }
-		 //printf ("\x1B[0m\n\n");
+		 
 }
